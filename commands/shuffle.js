1c02dc1c196f01367c22c58b8ad287f9c1a168d8
Andres Felipe Contreras Mendez <contrerandres001@gmail.com>
2025-07-01 20:38:05 -0500

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const isDJ = require('../../utils/isDJ');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,18 +7,22 @@ module.exports = {
     .setDescription('Mezcla el orden de las canciones en la cola (excepto la actual).'),
 
   async execute(interaction) {
+    if (!isDJ(interaction)) {
+      return interaction.reply({ content: '🎧 Solo usuarios con rol DJ pueden usar este comando.', ephemeral: true });
+    }
+
     const queue = interaction.client.queues.get(interaction.guild.id);
     if (!queue || queue.songs.length <= 1) {
       return interaction.reply('❌ No hay suficientes canciones para mezclar.');
     }
 
-    const [current, ...rest] = queue.songs;
-    for (let i = rest.length - 1; i > 0; i--) {
+    const [actual, ...resto] = queue.songs;
+    for (let i = resto.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [rest[i], rest[j]] = [rest[j], rest[i]];
+      [resto[i], resto[j]] = [resto[j], resto[i]];
     }
-    queue.songs = [current, ...rest];
 
+    queue.songs = [actual, ...resto];
     return interaction.reply('🔀 Cola mezclada.');
   }
 };
