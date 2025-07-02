e304b20ca0336ee7590ef675e3885355ed600d60
Andres Felipe Contreras Mendez <contrerandres001@gmail.com>
2025-07-01 20:21:24 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel]
+});
+
+// Colección de comandos slash
+client.commands = new Collection();
+
+// Cargar manejador de comandos
+const commandHandler = require('./handlers/commandHandler');
+commandHandler(client);
+
+// Cargar eventos
+const eventsPath = path.join(__dirname, 'events');
+fs.readdirSync(eventsPath).forEach(file => {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+});
+
+// Iniciar sesión
+client.login(process.env.DISCORD_TOKEN);
