0f3543c7339448ac308851e568f16af405e42010
Andres Felipe Contreras Mendez <contrerandres001@gmail.com>
2025-07-01 20:37:30 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getVoiceConnection } = require('@discordjs/voice');
+const isDJ = require('../../utils/isDJ');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,6 +8,10 @@ module.exports = {
     .setDescription('Detiene la música y limpia la cola.'),
 
   async execute(interaction) {
+    if (!isDJ(interaction)) {
+      return interaction.reply({ content: '🎧 Solo usuarios con rol DJ pueden usar este comando.', ephemeral: true });
+    }
+
     const guildId = interaction.guild.id;
     const connection = getVoiceConnection(guildId);
 
